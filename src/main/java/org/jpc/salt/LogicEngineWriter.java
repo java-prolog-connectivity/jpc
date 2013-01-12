40378e094870720eb849391bcebb6fb912c14da9
Sergio Castro <sergioc78@gmail.com>
2013-01-12 14:03:24 +0100

    changing most log messages from debug to trace in the class LogicEngineWriter

--- src/main/java/org/jpc/salt/LogicEngineWriter.java
+++ src/main/java/org/jpc/salt/LogicEngineWriter.java
@@ -21,7 +21,7 @@ public class LogicEngineWriter extends PrologWriter {
 		if(!getLogicEngine().query(directive).hasSolution())
 			logger.error("The directive: " + directive.toString(getLogicEngine()) + "cannot be executed");
 		else
-			logger.debug("The directive: " + directive.toString(getLogicEngine()) + " has been executed");
+			logger.trace("The directive: " + directive.toString(getLogicEngine()) + " has been executed");
 	}
 	
 	public void writeLogtalkObjectDirective(Term directive) {
@@ -29,14 +29,14 @@ public class LogicEngineWriter extends PrologWriter {
 		if(!logtalkObject.perform(directive).hasSolution())
 			logger.error("The logtalk object: " + logtalkObject.asTerm().toString(getLogicEngine()) + " cannot execute the directive: " + directive.toString(getLogicEngine()));
 		else
-			logger.debug("The logtalk object: " + logtalkObject.asTerm().toString(getLogicEngine()) + " has executed the directive: " + directive.toString(getLogicEngine()));
+			logger.trace("The logtalk object: " + logtalkObject.asTerm().toString(getLogicEngine()) + " has executed the directive: " + directive.toString(getLogicEngine()));
 	}
 	
 	public void writePrologClause(Term clause) {
 		if(!getLogicEngine().assertz(clause))
 			logger.error("Impossible to assert the clause: " + clause.toString(getLogicEngine()) + " in the logic database");
 		else
-			logger.debug("The clause: " + clause.toString(getLogicEngine()) + " has been asserted in the logic database");
+			logger.trace("The clause: " + clause.toString(getLogicEngine()) + " has been asserted in the logic database");
 	}
 	
 	public void writeLogtalkObjectClause(Term clause) {
@@ -45,7 +45,7 @@ public class LogicEngineWriter extends PrologWriter {
 			if(!logtalkObject.assertz(clause))
 				logger.error("Impossible to assert the clause: " + clause.toString(getLogicEngine()) + " in the logtalk object: " + logtalkObject.asTerm().toString(getLogicEngine()));
 			else
-				logger.debug("The clause: " + clause.toString(getLogicEngine()) + " has been asserted in the logtalk object: " + logtalkObject.asTerm().toString(getLogicEngine()));
+				logger.trace("The clause: " + clause.toString(getLogicEngine()) + " has been asserted in the logtalk object: " + logtalkObject.asTerm().toString(getLogicEngine()));
 		} catch(Exception e) {
 			//System.out.println(e);
 		}
