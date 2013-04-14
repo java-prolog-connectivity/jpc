39e70f6a1f88b2edb386072935e95fb228957058
Sergio Castro <sergioc78@gmail.com>
2013-04-15 01:17:47 +0200

    refactoring the map converter class

--- src/main/java/org/jpc/converter/catalog/listterm/MapConverter.java
+++ src/main/java/org/jpc/converter/catalog/listterm/MapConverter.java
@@ -1,9 +1,6 @@
 package org.jpc.converter.catalog.listterm;
 
-import static java.util.Arrays.asList;
-
 import java.lang.reflect.Type;
-import java.util.AbstractMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -11,8 +8,8 @@ import java.util.Map.Entry;
 import org.jpc.Jpc;
 import org.jpc.converter.JpcConversionException;
 import org.jpc.converter.JpcConverter;
-import org.jpc.converter.catalog.listterm.MapConverter.MapEntryConverter.MapEntryToTermConverter;
-import org.jpc.converter.catalog.listterm.MapConverter.MapEntryConverter.TermToMapEntryConverter;
+import org.jpc.converter.catalog.listterm.MapEntryConverter.MapEntryToTermConverter;
+import org.jpc.converter.catalog.listterm.MapEntryConverter.TermToMapEntryConverter;
 import org.jpc.converter.typesolver.MapTypeSolver;
 import org.jpc.term.Compound;
 import org.jpc.term.ListTerm;
@@ -105,82 +102,6 @@ public abstract class MapConverter<K,V> extends JpcConverter<Map<K,V>, Term> {
 	
 	
 	
-	public static abstract class MapEntryConverter<K,V> extends JpcConverter<Entry<K,V>, Compound> {
 
-		protected String entrySeparator;
-
-		public MapEntryConverter() {
-			this(MapTypeSolver.DEFAULT_MAP_ENTRY_SEPARATOR);
-		}
-		
-		public MapEntryConverter(String entrySeparator) {
-			this.entrySeparator = entrySeparator;
-		}
-		
-		@Override
-		public Entry<K,V> fromTerm(Compound term, Type type, Jpc context) {
-			if(!term.hasFunctor(entrySeparator, 2)) //verify the term structure
-				throw new JpcConversionException();
-			TypeWrapper typeWrapper = TypeWrapper.wrap(type);
-			if(!(typeWrapper.getRawClass().equals(Entry.class) || typeWrapper.getRawClass().equals(AbstractMap.SimpleEntry.class))) //verify the type
-				throw new JpcConversionException();
-			
-			Term keyTerm = term.arg(1);
-			Term valueTerm = term.arg(2);
-			Type[] entryTypes = TypeWrapper.wrap(type).as(Entry.class).getActualTypeArgumentsOrUpperBounds();
-			Type keyType = entryTypes[0];
-			Type valueType = entryTypes[1];
-			Object key = context.fromTerm(keyTerm, keyType);
-			Object value = context.fromTerm(valueTerm, valueType);
-			return new AbstractMap.SimpleEntry(key, value);
-		}
-		
-		@Override
-		public <T extends Compound> T toTerm(Entry<K,V> entry, Class<T> termClass, Jpc context) {
-			Term key = context.toTerm(entry.getKey());
-			Term value = context.toTerm(entry.getValue());
-			Compound term = new Compound(entrySeparator, asList(key, value));
-			return (T) term;
-		}
-		
-		
-		
-		
-		public static class MapEntryToTermConverter<K,V> extends MapEntryConverter<K,V> {
-			
-			public MapEntryToTermConverter() {
-				super();
-			}
-			
-			public MapEntryToTermConverter(String entrySeparator) {
-				super(entrySeparator);
-			}
-			
-			@Override
-			public Entry<K,V> fromTerm(Compound term, Type type, Jpc context) {
-				throw new UnsupportedOperationException();
-			}
-
-		}
-		
-		
-		public static class TermToMapEntryConverter<K,V> extends MapEntryConverter<K,V> {
-			
-			public TermToMapEntryConverter() {
-				super();
-			}
-			
-			public TermToMapEntryConverter(String entrySeparator) {
-				super(entrySeparator);
-			}
-
-			@Override
-			public <T extends Compound> T toTerm(Entry<K,V> entry, Class<T> termClass, Jpc context) {
-				throw new UnsupportedOperationException();
-			}
-			
-		}
-
-	}
 
 }
