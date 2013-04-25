abcb346ab41d2e78b5b04ea4dce0c12381bee605
Sergio Castro <sergioc78@gmail.com>
2013-04-25 23:02:51 +0200

    correcting a bug in the ExceptionHandledQuery class

--- src/main/java/org/jpc/query/ExceptionHandledQuery.java
+++ src/main/java/org/jpc/query/ExceptionHandledQuery.java
@@ -2,7 +2,6 @@ package org.jpc.query;
 
 import java.util.Map;
 
-import org.jpc.term.Compound;
 import org.jpc.term.Term;
 import org.jpc.term.Variable;
 
@@ -13,14 +12,13 @@ public class ExceptionHandledQuery extends QueryAdapter {
 	public static final String EXCEPTION_VAR_NAME = "JPC_EXCEPTION_VAR";
 	
 
-	
 	private final Function<Map<String,Term>, Map<String,Term>> exceptionAdapterFunction = new Function<Map<String,Term>, Map<String,Term>>() {
 		@Override
 		public Map<String,Term> apply(Map<String,Term> solution) {
 			if(solution.containsKey(EXCEPTION_VAR_NAME)) {
 				Term exceptionTerm = solution.get(EXCEPTION_VAR_NAME);
 				if(!(exceptionTerm instanceof Variable))
-					getJpcContext().handleError(getPrologEngine(), exceptionTerm, getGoal());
+					getJpcContext().handleError(exceptionTerm, getGoal());
 			}
 			return solution;
 		}
@@ -29,16 +27,6 @@ public class ExceptionHandledQuery extends QueryAdapter {
 	public ExceptionHandledQuery(Query query) {
 		super(query);
 		adapterFunction = exceptionAdapterFunction;
-	}
-
-	@Override
-	public Term getGoal() {
-		return query.getGoal();
-	}
-	
-	@Override
-	protected Term getDefaultSelectedTerm() {
-		return query.getGoal();
 	}
 	
 }
