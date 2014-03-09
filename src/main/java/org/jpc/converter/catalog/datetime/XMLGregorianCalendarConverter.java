f568359cff737c53532c9e2f5ec96eecc868efa4
Sergio Castro <sergioc78@gmail.com>
2014-03-09 11:33:35 +0100

    putting back the deleted converter to see if it is fixed now

---
+++ src/main/java/org/jpc/converter/catalog/datetime/XMLGregorianCalendarConverter.java
@@ -0,0 +1,32 @@
+package org.jpc.converter.catalog.datetime;
+
+import java.lang.reflect.Type;
+import java.util.GregorianCalendar;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import org.jpc.Jpc;
+import org.jpc.converter.FromTermConverter;
+import org.jpc.converter.ToTermConverter;
+import org.jpc.term.Term;
+
+public class XMLGregorianCalendarConverter<T extends Term> implements ToTermConverter<XMLGregorianCalendar, T>, FromTermConverter<T, XMLGregorianCalendar> {
+
+	@Override
+	public T toTerm(XMLGregorianCalendar calendar, Class<T> termClass, Jpc context) {
+		return context.toTerm(calendar.toGregorianCalendar(), termClass);
+	}
+
+	@Override
+	public XMLGregorianCalendar fromTerm(T term, Type targetType, Jpc context) {
+		GregorianCalendar gregCalendar = context.fromTerm(term, GregorianCalendar.class);
+		try {
+			return DatatypeFactory.newInstance().newXMLGregorianCalendar(gregCalendar);
+		} catch (DatatypeConfigurationException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+}
