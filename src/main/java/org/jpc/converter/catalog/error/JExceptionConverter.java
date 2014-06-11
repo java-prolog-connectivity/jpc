b99c75e3c487489f10458d2235998d738b651aa6
Sergio Castro <sergioc78@gmail.com>
2014-06-11 02:43:39 +0200

    improving on the representation of Java errors on the Prolog side

--- src/main/java/org/jpc/converter/catalog/error/JExceptionConverter.java
+++ src/main/java/org/jpc/converter/catalog/error/JExceptionConverter.java
@@ -1,13 +1,12 @@
 package org.jpc.converter.catalog.error;
 
 import static java.util.Arrays.asList;
+import static org.jpc.engine.prolog.PrologConstants.PROLOG_ERROR_FUNCTOR_NAME;
 
 import org.jpc.Jpc;
 import org.jpc.converter.ToTermConverter;
-import org.jpc.term.Atom;
 import org.jpc.term.Compound;
 import org.jpc.term.Term;
-import org.jpc.term.Var;
 
 public class JExceptionConverter implements ToTermConverter<Throwable, Compound> {
 
@@ -15,10 +14,12 @@ public class JExceptionConverter implements ToTermConverter<Throwable, Compound>
 	
 	@Override
 	public Compound toTerm(Throwable ex, Class<Compound> termClass, Jpc jpc) {
-		Term stackTraceTerm = jpc.toTerm(ex.getStackTrace());
+		Term throwableClassTerm = jpc.toTerm(ex.getClass());
+		Term messageTerm = jpc.toTerm(ex.getMessage());
 		Term causeTerm = jpc.toTerm(ex.getCause());
-		Term messageTerm = ex.getMessage() != null ? new Atom(ex.getMessage()) : Var.ANONYMOUS_VAR;
-		return new Compound(JEXCEPTION_FUNCTOR_NAME, asList(messageTerm, stackTraceTerm, causeTerm));
+		Term stackTraceTerm = jpc.toTerm(ex.getStackTrace());
+		Term formalDescriptiomTerm = new Compound(JEXCEPTION_FUNCTOR_NAME, asList(throwableClassTerm, messageTerm, causeTerm));
+		return new Compound(PROLOG_ERROR_FUNCTOR_NAME, asList(formalDescriptiomTerm, stackTraceTerm));
 	}
 
 }
